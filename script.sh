6c9f4777f9cad9ff49df07467965824b5f19f17d
Adimeisongit <adimeyao@gmail.com>
2022-06-26 20:50:57 +0000

    Starting commit

---
+++ script.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+echo "Welcome to the automatic Essay Generator"
+
+sleep 3
+
+echo "What is your name ?"
+
+read name
+
+sleep 1
+
+echo "what is your age ?"
+
+read age
+
+sleep 1
+
+echo "Where do you come from?"
+
+read origin
+
+echo ""
+
+read
+
+sleep 1
+
+read
+
+echo ""
+
+read
+
+sleep 1
+
+read
+
+echo ""
+
+read
+ 
+sleep 1
+
+echo ""
+                                                                                                                                                                                           
+sleep 1
+
+read
+
+sleep 1
+
+echo ""
+
+read
+
+sleep 1
+
+echo ""
+
+read 
+
+sleep 1
+
+echo ""
+
+read
+
+sleep 1
+
+echo ""
+
+read
+
+sleep 1
+
+echo ''
+
+read
+
+sleep 1
+
+
+echo "My name is $name, I am $age years old. " 
