27fb3c8a68a0ce35b3bcbad97a0bf836b8e9d0bb
Adime <96598659+Adimeisongit@users.noreply.github.com>
2023-04-24 23:56:08 +0000

    Update scrpy.py

--- scrpy.py
+++ scrpy.py
@@ -1,5 +1,8 @@
-print("Welcome to the Self examination tool")
-name = input("What is your name?: ")
-age = input("How old are you?: ")
-country = input("Which country do you live in?: ")
-print("My name is " + name + "," " I am " + str(age) + " years old." + " I live in " + country + ".")
+name = input("What is your name? ")
+age = input("How old are you? ")
+residence = input("Where do you live? ")
+hobbies = input("What are your hobbies? ")
+dream_job = input("What is your dream job? ")
+goal = input("What is your personal or professional goal? ")
+
+print(f"My name is {name}. I am {age} years old and I live in {residence}. My hobbies are {hobbies}. My dream job is to be a {dream_job} and my personal/professional goal is to {goal}.")
